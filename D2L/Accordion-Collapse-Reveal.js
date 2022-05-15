5cecb6c443d854c2ae86347d9e9ce04d15595b8b
corydave <corydave@users.noreply.github.com>
2022-05-14 20:02:04 -0400

    Update Accordion-Collapse-Reveal.js

--- D2L/Accordion-Collapse-Reveal.js
+++ D2L/Accordion-Collapse-Reveal.js
@@ -6,24 +6,25 @@ function revealOrHide() {
     } else {
         hideContent();
     }
+};
 
-    function showContent() {
+function showContent() {
 
-        for (var i = 0; i < document.getElementsByClassName('collapse').length; i++) {
-            console.log(document.getElementsByClassName('collapse')[i]);
-            document.getElementsByClassName('collapse')[i].classList.add('show');
-            document.getElementsByClassName('btn btn-link')[i].setAttribute('aria-expanded', 'true');
-            isCollapsed = !isCollapsed;
-        }
+    for (var i = 0; i < document.getElementsByClassName('collapse').length; i++) {
+        console.log(document.getElementsByClassName('collapse')[i]);
+        document.getElementsByClassName('collapse')[i].classList.add('show');
+        document.getElementsByClassName('btn btn-link')[i].setAttribute('aria-expanded', 'true');
+        isCollapsed = !isCollapsed;
+    }
 
-    };
+};
 
-    function hideContent {
+function hideContent {
 
-        for (var i = 0; i < document.getElementsByClassName('collapse').length; i++) {
-            console.log(document.getElementsByClassName('collapse')[i]);
-            document.getElementsByClassName('collapse')[i].classList.remove('show');
-            document.getElementsByClassName('btn btn-link')[i].setAttribute('aria-expanded', 'false');
-            isCollapsed = !isCollapsed;
-        }
-    };
+    for (var i = 0; i < document.getElementsByClassName('collapse').length; i++) {
+        console.log(document.getElementsByClassName('collapse')[i]);
+        document.getElementsByClassName('collapse')[i].classList.remove('show');
+        document.getElementsByClassName('btn btn-link')[i].setAttribute('aria-expanded', 'false');
+        isCollapsed = !isCollapsed;
+    }
+};
