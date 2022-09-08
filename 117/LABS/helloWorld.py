2059e20a7a229601dbb756f683ac36754e1d5325
corydave <corydave@users.noreply.github.com>
2022-09-07 20:49:53 -0400

    Update helloWorld.py

--- 117/LABS/helloWorld.py
+++ 117/LABS/helloWorld.py
@@ -1,1 +1,11 @@
+# Dave Ghidiu
+# September 6, 2022
+# This program outputs "Hello, World!" on the screen. Awesome.
+
 print("Hello, World!")
+
+# Note that:
+# 
+# print('Hello, World')
+#
+# also works
